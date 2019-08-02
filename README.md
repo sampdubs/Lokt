497a3f653e9543d60e892682767c95318d669a9e
Sam Prausnitz-Weinbaum <sammypw1@gmail.com>
2019-08-02 17:39:43 -0400

    Updates to startup script and better README

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # Lokt
-
 Lokt is a password manager build using two systems:
 - [fbs](https://build-system.fman.io/) along with PyQt5 in python3 for the desktop application
 - [React Native](https://facebook.github.io/react-native/) in JavaScript for the mobile application
+## Prerequisites
+- You need to have git in order to clone the repo
+- You should have the latest version of xcode installed (you can [get it on the app store](https://apps.apple.com/us/app/xcode/id497799835?mt=12)).
+- If you would like to run an android simulator, you can download android studio [here](https://developer.android.com/studio)
+## Setup
+To set everything up, ready for running and building, simply follow these steps:
+```
+git clone https://github.com/sampdubs/Lokt.git
+cd Lokt
+./startup.sh
+source Lokt-venv/bin/activate
+```
--- startup.sh
+++ startup.sh
@@ -7,7 +7,7 @@ while true; do
     read -p "${purple}Do you wish to continue? (y/n) ${normal}" yn
     case $yn in
         [Yy]* ) break;;
-        [Nn]* ) echo "${purple}Too bad, you wont be able build the app without this script ;)${normal}" && exit;;
+        [Nn]* ) echo "${purple}Too bad, this script sure does make making the app a whole lot easier ;)${normal}" && exit;;
         * ) echo "${purple}Please answer yes or no.${normal}";;
     esac
 done
@@ -55,14 +55,18 @@ pyenv uninstall -f 3.6.0
 PYTHON_CONFIGURE_OPTS="--enable-framework" pyenv install 3.6.0
 pyenv local 3.6.0
 pip install --upgrade pip
+echo "\n\n${purple}Setting up python virtual environment${normal}\n\n"
+pip install virtualenv
+virtualenv Lokt-venv
+source Lokt-venv/bin/activate
 
 mv server/* .
 pip install -Ur requirements.txt
-echo "\n\n${purple}Starting the fbs project. For app name, you should enter Lokt. You should just press enter through the next fields${normal}\n\n"
+echo "\n\n${purple}Starting the fbs project. For app name, you should enter Lokt. You can just press enter through the next fields${normal}\n\n"
 fbs startproject
-rm -rf src/main/icons/*
+rm -rf src/main/icons
 mkdir target
-mv lib/fbs/Icon.inconset target/Icon.inconset
+mv lib/fbs/Icon.iconset target/Icon.iconset
 mv lib/fbs/main.py src/main/python/main.py
 mv lib/fbs/helpers.py src/main/python/helpers.py
 echo "\n\n${purple}Starting the React Native project.${normal}\n\n"
@@ -76,3 +80,5 @@ yo rn-toolbox:assets --icon ../lib/react-native/icon.png
 mv ../lib/react-native/fernet.js node_modules/fernet/fernet.js
 cd ..
 rm -rf .git lib server startup.sh
+echo "\n\n${purple}Everything is setup! You can now run `fbs run` or `react-native run-(ios|android)`${normal}"
+echo "${purple}To learn more about usage and building for the app, go to https://build-system.fman.io/ and https://facebook.github.io/react-native/${normal}\n\n"
